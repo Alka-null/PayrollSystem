0e09848b8521d03f3c9a00afc47312e11e0bfe40
Alka-null <akanowo.ekarika@gmail.com>
2023-05-31 13:56:59 -0700

    update

---
+++ Services/ConfigurationEntities/BgServiceSettings.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Services.ConfigurationEntities
+{
+    public class BgServiceSettings
+    {
+        public string BaseUrl { get; set; }
+
+        public string Rate { get; set; }
+
+        public string Time { get; set; }
+    }
+}
